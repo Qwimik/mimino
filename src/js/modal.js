37387fefa6a70a303e754b535e90a8c43dfdb087
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-18 20:41:39 +0200

    edit modal.js file

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,20 +1,54 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  openBookRoomModalBtn: document.querySelector('[data-book-room-modal-open]'),
+  openBookTableModalBtn: document.querySelector('[data-book-table-modal-open]'),
+  openRoomPriceModalBtn: document.querySelector('[data-room-price-modal-open]'),
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.modal.addEventListener('click', e => {
-    if (e.currentTarget === e.target) {
-      toggleModal();
-    }
-  });
+  closeBookRoomModalBtn: document.querySelector('[data-book-room-modal-close]'),
+  closeBookTableModalBtn: document.querySelector(
+    '[data-book-table-modal-close]'
+  ),
+  closeRoomPriceModalBtn: document.querySelector(
+    '[data-room-price-modal-close]'
+  ),
 
-  function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+  bookRoomModal: document.querySelector('[data-book-room-modal]'),
+  bookTableModal: document.querySelector('[data-book-table-modal]'),
+  roomPriceModal: document.querySelector('[data-room-price-modal]'),
+};
+
+refs.openBookRoomModalBtn.addEventListener('click', toggleBookRoomModal);
+refs.openBookTableModalBtn.addEventListener('click', toogleBookTableModal);
+refs.openRoomPriceModalBtn.addEventListener('click', toogleRoomPriceModal);
+
+refs.closeBookRoomModalBtn.addEventListener('click', toggleBookRoomModal);
+refs.closeBookTableModalBtn.addEventListener('click', toogleBookTableModal);
+refs.closeRoomPriceModalBtn.addEventListener('click', toogleRoomPriceModal);
+
+refs.bookRoomModal.addEventListener('click', e => {
+  if (e.currentTarget === e.target) {
+    toggleBookRoomModal();
   }
-})();
+});
+refs.bookTableModal.addEventListener('click', e => {
+  if (e.currentTarget === e.target) {
+    toggleBookRoomModal();
+  }
+});
+refs.roomPriceModal.addEventListener('click', e => {
+  if (e.currentTarget === e.target) {
+    toogleRoomPriceModal();
+  }
+});
+
+function toggleBookRoomModal() {
+  document.body.classList.toggle('modal-open');
+  refs.bookRoomModal.classList.toggle('is-hidden');
+}
+function toogleBookTableModal() {
+  document.body.classList.toggle('modal-open');
+  refs.bookTableModal.classList.toggle('is-hidden');
+}
+function toogleRoomPriceModal() {
+  document.body.classList.toggle('modal-open');
+  refs.roomPriceModal.classList.toggle('is-hidden');
+}
